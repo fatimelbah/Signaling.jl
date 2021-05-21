8a389b3d9716cd9c289149dd63505f163e66a15e
Fatimetou El Bah <fatis.abd@hotmail.com>
2021-05-21 22:44:37 +0200

    updated

---
+++ README.md
@@ -0,0 +1,29 @@
+# Computational Economics : Final Project 
+
+### Group : Fatimetou El Bah, Veronika Micudova
+
+### Paper : Signaling to Experts (Pablo Kurlat and Florian Scheuer, 2017)
+
+
+<!-- [![](https://img.shields.io/badge/docs-stable-blue.svg)](https://fatimelbah.github.io/Signaling.jl/stable) -->
+[![](https://img.shields.io/badge/docs-dev-blue.svg)](https://fatimelbah.github.io/Signaling.jl/dev)
+
+GitHub Actions : [![Build Status](https://github.com/fatimelbah/Signaling.jl/workflows/CI/badge.svg)](https://github.com/fatimelbah/Signaling.jl/actions?query=workflow%3ACI+branch%3Amaster)
+
+
+[![Coverage Status](https://coveralls.io/repos/fatimelbah/Signaling.jl/badge.svg?branch=master)](https://coveralls.io/r/fatimelbah/Signaling.jl?branch=master)
+[![codecov.io](http://codecov.io/github/fatimelbah/Signaling.jl/coverage.svg?branch=master)](http://codecov.io/github/fatimelbah/Signaling.jl?branch=master)
+
+This package provides a `Julia` infrastructure to replicate the paper *[Signaling to Experts](https://www.nber.org/system/files/working_papers/w23817/w23817.pdf)* by Kurlat & Scheuer (2017).
+
+## Installation Instructions
+
+In your julia REPL type
+
+```julia
+] add https://github.com/fatimelbah/Signaling.jl.git
+```
+
+## Showcase
+
+This package replicate numerical simulations done in the paper and try to generate the figures presented in the paper and its *[Online Appendix](https://zenodo.org/record/4004262#.YKfP4JMzbQ0)*
--- src/Signaling.jl
+++ src/Signaling.jl
@@ -1,18 +1,178 @@
-module Signaling
-export hello, domath
+module SignalingToExperts
+
+export errmktclear, equil, f, indif, mktclear2
+
+#This file create the numerical simulations and functions used to generate figure 6
 
 """
-    hello(who::String)
+    We will use the following packages for our replication study
 
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
 
 """
-    domath(x::Number)
+using Roots, Plots, LaTeXStrings, Statistics, ColorSchemes, Calculus, QuadGK
 
-Return `x + 5`.
+###Parameters ###
+
+qH = 1
+qL = 0.4
+
+cH = .09
+cL = .15
+
+lambda = 0.6
+lambdahat = lambda
+Delta = 1
+
+##Parameters of F
+A = .5
+B = 13.3
+C = .4
+D = 2.8
+E = 2.2
+F = 0
+
+## defining estar and wstar
+
+estar = (qH - qL)/cL
+wstar = qH - cH*estar
+
+
+N = 100
+pis = LinRange(0.00001,.99999,N)
+
+N2 = 1000
+
 """
-domath(x::Number) = x + 5
+    We will now create the functions needed to plot the graphs for figure 6. 
+        These Functions are: errmktclear, equil, f, indif, mktclear2. 
+
+
+"""
+
+function errmktclear(pi::Any,theta::Any)
+
+
+    g(t)= Delta./((lambda-t) * lambdahat/lambda + pi *(1-lambdahat)) *f(t)
+   
+    y= quadgk(g,theta,lambda) ## this takes the integral of g
+
+      return y[1]-1 ## the quadgk function returns a vector where the first entry corresponds
+                    ##the value of the integral. hence the y[1]
+end
+
+
+function equil(pi::Any)
+
+    theta = indif(pi)
+
+    y = errmktclear(pi,theta)
+
+    return y
+end
+
+function f(theta::Any)
+
+   
+    y = A .*(theta>=0) .*(theta<=lambda) .* ((sin((B .*(lambda .- theta) .^C)+E .*pi)+1).^D + F) 
+    return y
+end
+
+function indif(pi::Any) #only one working for now
+        
+    y = lambda - pi .*lambdahat/lambda .*(1-lambdahat) .*1/(cL/cH-1)   
+        
+    return y
+end
+
+function mktclear2(pi::Any,guesstheta::Any)
+
+    if errmktclear(pi,0) < 0
+        y = 0
+    else() 
+        #guesstheta
+        y=find_zero( theta -> errmktclear(pi,theta),(0,guesstheta))
+    end 
+    
+    return y
+end 
+
+## Testing these functions gives us the same values as found in the matlab version.
+"""
+   Now that the functions are generated , we will define the parameters of interest. 
+
+"""
+global qH, qL, cH, cL, A, B, C, D, E, F, lambda, lambdahat, Delta  
+
+
+
+# Plot density
+
+thetas= zeros(N2)
+
+thetas = LinRange(0,lambda,N2)
+
+plot(thetas,f(thetas))
+
+## Plot equilibrium conditions
+
+guesstheta = lambda-0.03
+
+for j in 1:N
+    inds[j]= indif(pis[j])
+    mcs[j]= mktclear2(pis[j],guesstheta)
+    if j>1
+        guesstheta = mcs[j-1]
+    end
+end
+
+## I was not able to plot these as they were using an integrate matlab plots with latex 
+# that I unfortunately could not repllicate on time. 
+
+plot(pis,inds,"b',pis,mcs,'r','LineWidth",1.2) 
+
+leg = legend("Indifference','Market Clearing")
+
+xlabel("$\pi$','Interpreter', 'Latex','FontSize", 20) 
+ylabel("$\theta$','Interpreter', 'Latex','FontSize", 20)
+
+""" Find equilibria near hand-picked points"""
+find_zero( theta -> errmktclear(pi,theta),(0,guesstheta))
+pistar[1] = find_zero(pi-> equil(pi),(0,0.1)) 
+pistar[2] = find_zero(pi-> equil(pi),(0,0.9))
+
+for j = 1:2
+    thetastar[j] = mktclear2(pistar[j],guesstheta)
+    guesstheta = thetastar[j]
+end
+
+## Compute profits in equilibrium & deviation
+
+thetasdev[1,:]=range(thetastar[1],lambda,length=N)
+thetasdev[2,:]=range(thetastar[2],lambda,length=N)
+
+profits[1,:] = ((lambda - thetasdev[1,:])*qL + pistar[1]*(1-lambda)*qH)./ (lambda - thetasdev[1,:] + pistar[1]*(1-lambda)) .-wstar
+
+profits[2,:] = ((lambda - thetasdev[2,:])*qL + pistar[2]*(1-lambda)*qH)./ (lambda - thetasdev[2,:] + pistar[2]*(1-lambda)) .-wstar
+
+for j = 1:N
+
+    a= quadgk(1 ./ (pistar[1]*(1-lambda)+lambda-t) .*f(t),thetastar[1],thetasdev[1,j])
+    b= quadgk(1 ./(pistar[2]*(1-lambda)+lambda-t) .*f(t),thetastar[2],thetasdev[2,j])
+
+    wD[1,j] = qL + (qH - qL) * (1-cH/cL * a[1]) ## multiply with only the first output of the integral
+
+    devprofits[1,j] = qH -wD[1,j]
+
+    wD[2,j] = qL + (qH - qL) * (1-cH/cL * b[1]) 
+    
+    devprofits[2,j] = qH -wD[2,j]
+end
+
+plot(thetasdev[1,:],profits[1,:],thetasdev[1,:],devprofits[1,:])
+legend("eq','dev")
+
+plot(thetasdev[2,:],profits[2,:],thetasdev[2,:],devprofits[2,:])
+legend("eq','dev")
+
 
 end
